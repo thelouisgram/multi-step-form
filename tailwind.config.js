0881d45b740c58a6c254ddb437bcc2f848a2dee6
thelouisgram <hadesanoye01@gmail.com>
2023-04-20 19:48:11 +0100

    set up style-guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,37 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        /* Blue shades */
+        marineBlue: 'hsl(213, 96%, 18%)',
+        purplishblue: 'hsl(243, 100%, 62%)',
+        pastelBlue: 'hsl(228, 100%, 84%)',
+        lightBlue: 'hsl(206, 94%, 87%)',
+
+        /* Red shades */
+        strawberryRed: 'hsl(354, 84%, 57%)',
+
+        /* Gray shades */
+        coolGray: 'hsl(231, 11%, 63%)',
+        lightGray: 'hsl(229, 24%, 87%)',
+        magnolia: 'hsl(217, 100%, 97%)',
+        alabaster: 'hsl(231, 100%, 99%)',
+        white: 'hsl(0, 0%, 100%)',
+
+      },
+      fontFamily: {
+        Ubuntu: ['Ubuntu', 'sans-serif']
+      },
+      screens: {
+        xs: '480px',
+        ss: '620px',
+        sm: '768px',
+        md: '1060px',
+        lg: '1200px',
+        xl: '1700px'
+      }
+    },
   },
   plugins: [],
 }
